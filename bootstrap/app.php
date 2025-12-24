8a6231d27703d83af92441ca28e6be83e12dac26
neobama <neorafa@Elon-Musks-Macbook-Pro.local>
2025-12-25 03:31:36 +0700

    Fix TrustProxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +14,14 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
+    ->withTrustedProxies(
+        at: '*', // Trust all proxies (Nginx Proxy Manager)
+        headers: Request::HEADER_X_FORWARDED_FOR |
+                 Request::HEADER_X_FORWARDED_HOST |
+                 Request::HEADER_X_FORWARDED_PORT |
+                 Request::HEADER_X_FORWARDED_PROTO |
+                 Request::HEADER_X_FORWARDED_AWS_ELB
+    )
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
