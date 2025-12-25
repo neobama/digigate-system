2e0ee9a942fd7e158beba3b5a29bc137abfd4028
neobama <neorafa@Elon-Musks-Macbook-Pro.local>
2025-12-25 11:38:37 +0700

    Minor Changes

--- app/Filament/Employee/Pages/Dashboard.php
+++ app/Filament/Employee/Pages/Dashboard.php
@@ -7,6 +7,7 @@ use Filament\Pages\Dashboard as BaseDashboard;
 class Dashboard extends BaseDashboard
 {
     protected static ?string $navigationLabel = 'Dashboard';
+    protected static ?string $title = 'Dashboard';
     
     public function getWidgets(): array
     {
