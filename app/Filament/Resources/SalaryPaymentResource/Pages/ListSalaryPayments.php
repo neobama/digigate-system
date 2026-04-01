9a172cdb65be96a451173a3c7f3a6f9ccac7f184
neobama <neorafa@Elon-Musks-Macbook-Pro.local>
2026-04-01 19:30:47 +0700

    Feature: Add feature slip gaji

--- app/Filament/Resources/SalaryPaymentResource/Pages/ListSalaryPayments.php
+++ app/Filament/Resources/SalaryPaymentResource/Pages/ListSalaryPayments.php
@@ -3,10 +3,18 @@
 namespace App\Filament\Resources\SalaryPaymentResource\Pages;
 
 use App\Filament\Resources\SalaryPaymentResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListSalaryPayments extends ListRecords
 {
     protected static string $resource = SalaryPaymentResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
 
